c980f28f89b0afeb372652082b50a69e748a4413
desongyu <desong.e.yu@gmail.com>
2017-02-22 09:19:24 -0500

    creat inital table

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+"use strict";
+ 
+module.exports = {
+  entry: './main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+ 
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
